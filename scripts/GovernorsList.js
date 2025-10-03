403b1991aa37ed57e70e8db9aa110f7b4c5db1ce
Lea Edwards <leaxedw@gmail.com>
2025-10-03 18:58:42 -0500

    added functions to the governors module

--- scripts/GovernorsList.js
+++ scripts/GovernorsList.js
@@ -1,8 +1,47 @@
 // import setter function from TransientState.js
 
+ import { setGovernor } from "./TransientState.js"
+ 
 // Make function handleGovernorChoice function
 
+const handleGovernorChoice = async (event) => {
+    if (event.target.id === "governors") {
+        let updatedGovernorOption = parseInt(event.target.value)
+        setGovernor(updatedGovernorOption)
+        console.log(updatedGovernorOption)
+    }
+}
 // Make function GovernorChoice async function 
+
+export const GovernorChoice = async () => {
+    const response = await fetch("http://localhost:8088/governors");
+    const governors = await response.json()
+
+    // Add event listener
+
+    document.addEventListener("change", handleGovernorChoice)
+
+    let html = `<h2>Governors</h2>
+                    <select required name="governor" id="governors-dropdown">
+                        <option value="0" disabled selected> Select One </option>
+                    `
+
+    const governorsHTML = governors.map((governor) => {
+        return `
+                <option 
+                    name="governor" 
+                    value="${governor.id}" 
+                    data-price="${governor.price}"> 
+                        ${governor.name} </option>
+            `
+        ;
+    });
+
+    html += governorsHTML.join("")
+    html += `</select>`
+
+    return html;
+}
 
 // This function will do an API call to fetch both governor data
 
